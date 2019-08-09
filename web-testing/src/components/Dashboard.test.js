120c25c2b68c9a12ffee6659c270e43fda59deae
antilou86 <antilou86@gmail.com>
2019-08-08 23:44:15 -0400

    playing around with testing

--- web-testing/src/components/Dashboard.test.js
+++ web-testing/src/components/Dashboard.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, cleanup} from '@testing-library/react'
+import {render, cleanup, fireEvent, getByText } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 import Dashboard from './Dashboard';
 
@@ -22,5 +22,34 @@ describe('<Dashboard/>', () => {
       expect(ballButton).toBeInTheDocument();
       expect(foulButton).toBeInTheDocument();
       expect(hitButton).toBeInTheDocument();
+    }),
+    it('should change state when buttons are clicked', () => {
+      const wrapper = render(<Dashboard/>)
+      
+
+      const strikeButton = wrapper.queryByTestId(/strikeButton/i);
+      const ballButton = wrapper.queryByTestId(/ballButton/i);
+      const foulButton = wrapper.queryByTestId(/foulButton/i);
+      const hitButton = wrapper.queryByTestId(/hitButton/i);
+
+      fireEvent.click(strikeButton)
+      wrapper.getByText(/strike: 1/)
+
+      fireEvent.click(ballButton)
+      wrapper.getByText(/ball: 1/)
+
+      fireEvent.click(hitButton)
+      wrapper.getByText(/strike: 0/)
+      wrapper.getByText(/ball: 0/)
+
+      fireEvent.click(strikeButton)
+      fireEvent.click(strikeButton)
+      fireEvent.click(strikeButton)
+      wrapper.getByText(/strike: 0/)
+
+      // fireEvent.click(ballButton)
+      // fireEvent.click(foulButton)
+      // fireEvent.click(hitButton)
+  
     })
 })
